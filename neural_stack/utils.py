ffc7263730c9425a14ee1018883cd8572ffa18da
andreifurdui <andreifurdui.g@gmail.com>
2025-12-03 12:15:59 +0200

    complexity computation

---
+++ neural_stack/utils.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+from typing import Tuple
+from fvcore.nn import FlopCountAnalysis, ActivationCountAnalysis, flop_count_table
+
+def count_model_params(model: nn.Module):
+    return sum(p.numel() for p in model.parameters())
+
+def model_summary(model: nn.Module, inputs: torch.Tensor | Tuple[torch.Tensor, ...]):
+    flops = FlopCountAnalysis(model, inputs)
+    acts = ActivationCountAnalysis(model, inputs)
+
+    num_params = count_model_params(model)
+    num_flops = flops.total()
+    num_activations = acts.total()
+
+    summary = flop_count_table(flops, activations=acts)
+
+    return num_params, num_flops, num_activations, summary
